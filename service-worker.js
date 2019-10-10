aaad5e03835c61f3da9d3c3481e9e52ffd2aeb9c
GitHub <noreply@github.com>
2019-10-10 14:11:36 +0000

    eb72dce fix(deps): bundle material-design-icons (DHIS2-5625) (v33)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.18a9097360996413772c3d0ee89c671b.js"
+  "./precache-manifest.8c30d7893aa414010f3e79876fe9cda8.js"
 );
 
 self.addEventListener('message', (event) => {
